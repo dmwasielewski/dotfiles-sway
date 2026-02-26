6a7fe2def1f874ad9de93b9a330f64bd92f12099
dmwasielewski <wasielewski.damian@gmail.com>
2026-02-26 00:47:47 +0000

    feat: automate symlinks and Flatpak install in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,24 @@
+
 #!/bin/bash
+set -e
+
+DOTFILES="$HOME/dotfiles-sway"
+
+echo "==> Creating config directories..."
+mkdir -p ~/.config/sway
+mkdir -p ~/.config/waybar
+mkdir -p ~/.config/foot
+
+
+# Symlinks
+echo "==> Creating symlinks..."
+ln -sf "$DOTFILES/sway/config"        ~/.config/sway/config
+ln -sf "$DOTFILES/waybar/config"      ~/.config/waybar/config
+ln -sf "$DOTFILES/foot/foot.ini"      ~/.config/foot/foot.ini
+ln -sf "$DOTFILES/user-dirs.dirs"     ~/.config/user-dirs.dirs
+
 # Flatpaks
-faltpak install -y flathub com.vivaldi.Vivaldi
+echo "==> Installing Flatpaks..."
+flatpak install -y flathub com.vivaldi.Vivaldi
+
+echo "==> Done."
